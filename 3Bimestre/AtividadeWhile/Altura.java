8376d55814075ca0cbd43fb06ea6ade0f8d69344
Liv288 <liveira026@gmail.com>
2024-09-07 14:00:32 -0300

    Add files via upload

---
+++ 3Bimestre/AtividadeWhile/Altura.java
@@ -0,0 +1,13 @@
+
+public class Altura {
+	public static void main (String[] args) {
+		int p=145, i=0;
+		double j=134;
+		
+		while(j<=p){
+			j=j+2.5;
+			p=p+2;
+			i++;
+		}System.out.println("João levará " +i+ " anos para ficar mais alto que Pedro.");
+	}
+}
